6358639722c807e15a71a65a44f63c3e9c162013
mohit579-pixel <114941107+mohit579-pixel@users.noreply.github.com>
2024-02-12 21:57:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 
 const store=MongoStore.create({
